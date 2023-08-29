873da6ed21a68cff682fbb3cc7b1ff8c62a6ba34
Skandar Munir Ahmed <sikandernoori@gmail.com>
2023-08-29 03:05:09 +0300

    update main code

--- lib/src/geo_elevation_lookup.dart
+++ lib/src/geo_elevation_lookup.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:geo_elevation_lookup/src/cache/tiff_image_cache.dart';
-import 'package:geo_elevation_lookup/src/utils/geo_utils.dart';
 import 'package:image/image.dart';
 
 /// A class that provides elevation data lookup based on geographic coordinates.
@@ -18,10 +17,10 @@ class GeoElevationLookup {
   final String _demsPath;
 
   /// Longitudinal sepration for each rectangler taken from [GTOPO30].
-  static const int dLon = 10;
+  static const int dLon = 4;
 
   /// Latitudinal sepration for each rectangler taken from [GTOPO30].
-  static const int dLat = 5;
+  static const int dLat = 2;
 
   /// Height of [GTOPO30] tiff file.
   static const int height = 180;
@@ -45,8 +44,10 @@ class GeoElevationLookup {
   GeoElevationLookup(
     this._demsPath, {
     this.enableCaching = true,
-    this.maxCacheSize = 10,
-  }) : tiffImageCache = TiffImageCache(maxCacheSize);
+    this.maxCacheSize = 100,
+  }) : tiffImageCache = TiffImageCache(maxCacheSize) {
+    if (!(Directory(_demsPath).existsSync())) throw 'DEM not found at path: $_demsPath';
+  }
 
   /// Get elevation from latitude and longitude.
   ///
@@ -56,22 +57,19 @@ class GeoElevationLookup {
   /// If the elevation data is not available, 0 is returned.
   Future<int> getElevation(double latitude, double longitude) async {
     try {
-      if (!(await Directory(_demsPath).exists())) throw 'DEM not found at path: $_demsPath';
+      var colAbsolute = longitudeToColumn(longitude);
+      var rowAbsolute = latitudeToRow(latitude);
+      var minLong = roundToPrevious4th(longitude.floor()).toDouble();
+      var colOffset = longitudeToColumn(minLong);
+      var maxLat = roundToNext2nd(latitude).toDouble();
+      var rowOffset = latitudeToRow(maxLat);
 
-      var colAbsolute = GeoUtils.longitudeToColumn(longitude);
-      var rowAbsolute = GeoUtils.latitudeToRow(latitude);
-      var significantLong = GeoUtils.roundToPrevious10th(longitude.floor()).toDouble();
-      var colOffset = GeoUtils.longitudeToColumn(significantLong);
-      var significantLat = GeoUtils.roundToNext5th(latitude).toDouble();
-      var rowOffset = GeoUtils.latitudeToRow(significantLat);
-
-      var index = ((GeoUtils.roundToPrevious10th(longitude.floor()) + (width / 2)) / dLon).floor() +
-          (((GeoUtils.roundToPrevious5th(latitude.floor()) + (height / 2)) / dLat) * (height / dLat)).floor();
+      var index = ((roundToPrevious4th(longitude.floor()) + (width / 2)) / dLon).floor() +
+          (((roundToPrevious2nd(latitude.floor()) + (height / 2)) / dLat) * (height / dLat)).floor();
 
       if (await File('$_demsPath/$index.tiff').exists()) {
-        var image = (tiffImageCache.get(index) as Image?) ??
-            TiffDecoder().decode(await File('$_demsPath/$index.tiff').readAsBytes());
-
+        var tiffFile = await File('$_demsPath/$index.tiff').readAsBytes();
+        var image = (tiffImageCache.get(index) as Image?) ?? TiffDecoder().decode(tiffFile);
         if (image == null) {
           throw 'Unable to parse file to image';
         } else if (image.data == null) {
@@ -82,12 +80,68 @@ class GeoElevationLookup {
           tiffImageCache.put(index, image);
         }
 
-        return image.data!.getPixel(colAbsolute - colOffset, (rowAbsolute - rowOffset).abs()).r.toInt();
+        double col = (colAbsolute - colOffset);
+        if (col < 21600) {
+          col = col.floor().toDouble();
+        } else {
+          col = col.round().toDouble();
+        }
+
+        double row = (rowAbsolute - rowOffset).abs();
+        if (row < 10800) {
+          row = row.floor().toDouble();
+        } else {
+          row = row.round().toDouble();
+        }
+
+        var e1 = image.data!.getPixel(col.toInt(), row.toInt()).r.toInt();
+        if (e1 == -32768) {
+          e1 = 0;
+        }
+
+        return e1;
       }
 
       return 0;
     } catch (_) {
       rethrow;
+    }
+  }
+
+  static double longitudeToColumn(double longitude) {
+    const double columnsPerDegree = 43200 / 360.000019;
+    return (columnsPerDegree * (longitude + (360.000019 / 2)));
+  }
+
+  static double latitudeToRow(double latitude) {
+    const double rowsPerDegree = 21600 / 180.00001;
+    return (rowsPerDegree * ((180.00001 / 2) - latitude));
+  }
+
+  // TODO: find a meaningful name
+  static int roundToPrevious4th(int value) {
+    if (value >= 0) {
+      return (value ~/ dLon) * dLon;
+    } else {
+      return ((value - (dLon - 1)) ~/ dLon) * dLon;
+    }
+  }
+
+  // TODO: find a meaningful name
+  static int roundToPrevious2nd(int value) {
+    if (value >= 0) {
+      return (value ~/ dLat) * dLat;
+    } else {
+      return ((value - (dLat - 1)) ~/ dLat) * dLat;
+    }
+  }
+
+  // TODO: find a meaningful name
+  static int roundToNext2nd(double value) {
+    if (value >= 0) {
+      return ((value + (dLat - 1)).ceil() ~/ dLat) * dLat;
+    } else {
+      return (value ~/ dLat) * dLat;
     }
   }
 }
