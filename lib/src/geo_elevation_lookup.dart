83f8d54984f34a249399278e486207b99cebcedd
Skandar Munir Ahmed <sikandernoori@gmail.com>
2023-09-06 18:32:53 +0300

    get elevation synchronously

--- lib/src/geo_elevation_lookup.dart
+++ lib/src/geo_elevation_lookup.dart
@@ -115,6 +115,69 @@ class GeoElevationLookup {
     }
   }
 
+  /// Get elevation synchronously from latitude and longitude.
+  ///
+  /// Returns the elevation in meters at the specified [latitude] and [longitude]
+  /// using data from the USGS EROS GTOPO30 DEM.
+  ///
+  /// If the elevation data is not available, 0 is returned.
+  int getElevationSync(double latitude, double longitude) {
+    try {
+      List<int> pixelCoords = latLongToXY(longitude, latitude);
+
+      var xAbsolute = pixelCoords[0];
+      var yAbsolute = pixelCoords[1];
+
+      var tiffStartX = getTiffStartX(xAbsolute);
+      var tiffStartY = getTiffStartY(yAbsolute).abs();
+
+      var tiffEndX = tiffStartX + imageWidth;
+      var tiffEndY = tiffStartY + imageHeight;
+
+      var fileName = '${tiffStartX}_$tiffStartY';
+
+      var xBefore = xAbsolute.toInt() - tiffStartX;
+      var yBefore = yAbsolute.toInt() - tiffStartY;
+
+      int x, y = 0;
+
+      if ((xAbsolute.floor() - xBefore) + imageWidth == tiffEndX) {
+        x = xAbsolute.floor() - tiffStartX;
+      } else {
+        x = xAbsolute.ceil() - tiffStartX;
+      }
+
+      if ((yAbsolute.floor() - yBefore) + imageHeight == tiffEndY) {
+        y = yAbsolute.floor() - tiffStartY;
+      } else {
+        y = yAbsolute.ceil() - tiffStartY;
+      }
+
+      if (File('$_demsPath/$fileName.tiff').existsSync()) {
+        var tiffFile = File('$_demsPath/$fileName.tiff').readAsBytesSync();
+        var image = (tiffImageCache.get(fileName) as Image?) ?? TiffDecoder().decode(tiffFile);
+        if (image == null) {
+          throw 'Unable to parse file to image';
+        } else if (image.data == null) {
+          throw 'Empty Image found at path: $_demsPath$fileName.tiff';
+        }
+
+        if (tiffImageCache.get(fileName) == null) {
+          tiffImageCache.put(fileName, image);
+        }
+
+        var elevation = image.data!.getPixel(x, y).r.toInt();
+        if (elevation == -32768) return 0;
+
+        return elevation;
+      }
+
+      return 0;
+    } catch (_) {
+      rethrow;
+    }
+  }
+
   int getTiffStartX(int col) => (col ~/ imageWidth) * imageWidth;
 
   int getTiffStartY(int row) => (row ~/ imageHeight) * imageHeight;
